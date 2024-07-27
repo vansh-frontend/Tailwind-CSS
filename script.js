84dcb59d04dfc5b9d1d7177f7dedc7d45024e269
vansh-frontend <vanshdhalor04@gmail.com>
2024-07-27 22:14:49 +0530

    first commit

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ tailwind.config = {
     },
   },
 };
+
+
+document.getElementById("show-signup").addEventListener("click", function () {
+  document.getElementById("login-form").classList.add("hidden");
+  document.getElementById("signup-form").classList.remove("hidden");
+});
+
+document.getElementById("show-login").addEventListener("click", function () {
+  document.getElementById("signup-form").classList.add("hidden");
+  document.getElementById("login-form").classList.remove("hidden");
+});
